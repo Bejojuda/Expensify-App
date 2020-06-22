b39f84d35b1996c5c1106deaa097bcfe9124eebd
Juan <juanma.712@hotmail.com>
2020-06-22 13:37:20 -0500

    Configuración de devDependencies y folder dist

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = (env) => {
 	return {
 		entry: './src/app.js',			//Archivo con el que se inicia webpack
 		output: {
-			path: path.join(__dirname, 'public'), 
+			path: path.join(__dirname, 'public', 'dist'), 
 			filename: 'bundle.js'
 		},
 		mode: 'development',
@@ -60,7 +60,8 @@ module.exports = (env) => {
 		devtool: isProduction? 'source-map' : 'inline-source-map',
 		devServer: {
 			contentBase: path.join(__dirname, 'public'),
-			historyApiFallback: true
+			historyApiFallback: true,
+			publicPath: '/dist/'
 		}
 	}
 };
